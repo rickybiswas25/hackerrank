521a57f3c637495ec9f209e355a837c06a5e09d5
deb <debmalya.biswas25@gmail.com>
2020-05-10 18:54:48 +0530

    Added page action example

---
+++ pageAction/background.js
@@ -0,0 +1,19 @@
+browser.tabs.onUpdated.addListener(tabId => {
+    initialize(tabId);
+});
+
+function initialize(tabId){
+    browser.pageAction.setIcon({
+        tabId: tabId,
+        path: "icons/logo.png"
+    })
+    browser.pageAction.setTitle({
+        tabId: tabId,
+        title: "MY ACTION"
+    });
+    browser.pageAction.show(tabId);
+    browser.pageAction.setPopup({
+        tabId: tabId,
+        popup: "index.html"
+    })
+}
